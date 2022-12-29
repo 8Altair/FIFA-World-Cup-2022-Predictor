e8458a302cd2a8a635c1237c27c0bbb3bd125679
Altair <dino.ahmetbegovic@gmail.com>
2022-12-30 00:29:52 +0100

    Update

--- Quarter_finals_parser.py
+++ Quarter_finals_parser.py
@@ -7,12 +7,14 @@ def quarter_finals_file_parser(text, round_of_16_predictions):
         row_number = 0
         key = ""
         for row in quarter_finals:
+            print(round_of_16_predictions)
+            print(row_number, row)
             if row_number % 4 == 0:
                 key = row[:4] + row[7:-1]
             if row_number % 4 == 2:
                 winner = row[len(round_of_16_predictions[key[:4]][0]) +
-                             len(round_of_16_predictions[key[4:]][0]) + 8:-1]
-
+                             len(round_of_16_predictions[key[4:]][0]) + 8:-1]   # Ovde pogledaj.
+                print(round_of_16_predictions[key[:4]][0], round_of_16_predictions[key[4:]][0])
                 if row[-1] != "\n":
                     winner += row[-1]
                 validation_check(winner)
@@ -26,6 +28,5 @@ def quarter_finals_file_parser(text, round_of_16_predictions):
                 predictions[key] = (winner, runner_up)
 
             row_number += 1
-            print(row_number, row)
 
     return predictions
