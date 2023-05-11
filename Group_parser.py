7e70998f644a0b65a84eb01c5cb38d79016fcceb
Altair <dino.ahmetbegovic@gmail.com>
2023-05-11 23:52:11 +0200

    Parallel file reading

--- Group_parser.py
+++ Group_parser.py
@@ -20,23 +20,23 @@ def validation_check(group, country_1, country_2):
 
 def group_stage_file_parser(file_path):
     predictions = {}
-    with open(file_path, "r") as group_stage:
+    with open(file_path, "r") as group_stage, open("Rezultati grupe.txt", "r") as results:
         row_number = 0
         key_part_one = "Group "
         key_part_two = chr(ord("A") - 1)
         group_name = ""
         first_country = ""
 
-        for row in group_stage:
+        for row_1, row_2 in zip(group_stage, results):
             if row_number % 5 == 0:
                 key_part_two = chr(ord(key_part_two) + 1)
                 group_name = key_part_one + key_part_two
             if row_number % 5 == 2:
-                first_country = row[3:-1]
+                first_country = row_1[3:-1]
             if row_number % 5 == 3:
-                second_country = row[3:-1]
-                if row[-1] != "\n":
-                    second_country += row[-1]
+                second_country = row_1[3:-1]
+                if row_1[-1] != "\n":
+                    second_country += row_1[-1]
                 validation_check(group_name, first_country, second_country)
                 predictions[group_name] = (first_country, second_country)
             row_number += 1
