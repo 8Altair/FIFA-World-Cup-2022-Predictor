2f0936649dc753bc074aeba1a4f1d450ac63a98b
Altair <dino.ahmetbegovic@gmail.com>
2023-05-29 01:40:29 +0200

    Working on further calculations

--- Round_of_16_parser.py
+++ Round_of_16_parser.py
@@ -3,27 +3,47 @@ from Validation import validation_check
 
 def round_of_16_file_parser(file_path, group_predictions):
     predictions = {}
-    with open(file_path, "r") as round_of_16:
+    with open(file_path, "r") as round_of_16, open("Rezultati osmine finala.txt", "r") as results:
         row_number = 0
         key_name = ""
-        for row in round_of_16:
-            if row_number % 4 == 0:
-                key_name = row[:2] + row[5:-1]
-            if row_number % 4 == 2:
-                winner = row[len(group_predictions["Group " + key_name[0]][0]) +
-                             len(group_predictions["Group " + key_name[2]][1]) + 8:-1]
+        first_country = ""
+        winner = ""
 
-                if row[-1] != "\n":
-                    winner += row[-1]
+        for file_1, file_2 in zip(round_of_16, results):
+            if row_number % 4 == 0:
+                key_name = file_1[:2] + file_1[5:-1]
+            if row_number % 4 == 2:
+                first_country = file_1[len(group_predictions["Group " + key_name[0]][0]) +
+                                       len(group_predictions["Group " + key_name[2]][1]) + 8:-1]
+                winner = file_2[len(group_predictions["Group " + key_name[0]][0]) +
+                                len(group_predictions["Group " + key_name[2]][1]) + 8:-1]
+
+                if file_1[-1] != "\n":
+                    first_country += file_1[-1]
+                validation_check(first_country)
+                if file_2[-1] != "\n":
+                    winner += file_2[-1]
                 validation_check(winner)
+                # Kako napraviti?
+                if group_predictions["Group " + key_name[0]][0] == first_country:
+                    second_country = group_predictions["Group " + key_name[2]][1]
+                else:
+                    second_country = group_predictions["Group " + key_name[0]][0]
+                validation_check(second_country)
 
                 if group_predictions["Group " + key_name[0]][0] == winner:
-                    runner_up = group_predictions["Group " + key_name[2]][1]
+                    loser = group_predictions["Group " + key_name[2]][1]
                 else:
-                    runner_up = group_predictions["Group " + key_name[0]][0]
-                validation_check(runner_up)
-                predictions[key_name] = (winner, runner_up)
+                    loser = group_predictions["Group " + key_name[0]][0]
+                validation_check(loser)
 
+                points = 0
+                if first_country == winner or second_country == winner:
+                    points += 1
+                if second_country == loser or first_country == loser:
+                    points += 1
+
+                predictions[key_name] = (winner, loser, points)
             row_number += 1
 
     return predictions
