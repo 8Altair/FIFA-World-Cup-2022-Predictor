e78c9531aeb83d0b7d43f881768d1121db0a0be3
Altair <dino.ahmetbegovic@gmail.com>
2023-07-05 00:41:20 +0200

    Finished parsing.

--- Final_parser.py
+++ Final_parser.py
@@ -2,20 +2,40 @@ from Validation import validation_check
 
 
 def final_file_parser(file_path, semi_finals_prediction):
-    with open(file_path, "r") as final:
-        row = final.readline()
-        key_name = row[:16] + row[19:-1]
-        row = final.readline()
-        row = final.readline()
-        winner_prediction = row[len(semi_finals_prediction[key_name[:16]][0]) +
-                                len(semi_finals_prediction[key_name[16:]][0]) + 8:-1]
-        winner_prediction += row[-1]
+    with open(file_path, "r") as final, open("Rezultati polufinala.txt", "r") as results:
+        file_1 = final.readline()
+        file_2 = results.readline()
+        key_name = file_1[:16] + file_1[19:-1]
+        file_1 = final.readline()
+        file_2 = results.readline()
+        file_1 = final.readline()
+        file_2 = results.readline()
 
-    validation_check(winner_prediction)
-    if semi_finals_prediction[key_name[:16]][0] == winner_prediction:
-        runner_up = semi_finals_prediction[key_name[16:]][0]
+        first_country = file_1[len(semi_finals_prediction[key_name[:16]][0]) +
+                               len(semi_finals_prediction[key_name[16:]][0]) + 8:-1]
+
+        first_country += file_1[-1]
+        row = file_2.split("-->")
+        winner = row[-1].strip().strip("\n")
+        validation_check(first_country)
+        validation_check(winner)
+
+    if semi_finals_prediction[key_name[:16]][0] == first_country:
+        second_country = semi_finals_prediction[key_name[16:]][0]
     else:
-        runner_up = semi_finals_prediction[key_name[:16]][0]
-    validation_check(runner_up)
+        second_country = semi_finals_prediction[key_name[:16]][0]
+    validation_check(second_country)
 
-    return winner_prediction, runner_up
+    if row[0].split("v")[0][:-1] == winner:
+        loser = row[0].split("v")[1][1:-1]
+    else:
+        loser = row[0].split("v")[0][:-1]
+    validation_check(loser)
+
+    points = 0
+    if first_country == winner or second_country == winner:
+        points += 1
+    if second_country == loser or first_country == loser:
+        points += 1
+
+    return first_country, second_country, points
--- Semi_finals_parser.py
+++ Semi_finals_parser.py
@@ -3,27 +3,44 @@ from Validation import validation_check
 
 def semi_finals_file_parser(file_path, quarter_finals_predictions):
     predictions = {}
-    with open(file_path, "r") as semi_finals:
+    with open(file_path, "r") as semi_finals, open("Rezultati polufinala.txt", "r") as results:
         row_number = 0
         key_name = ""
-        for row in semi_finals:
-            if row_number % 4 == 0:
-                key_name = row[:8] + row[11:-1]
-            if row_number % 4 == 2:
-                winner = row[len(quarter_finals_predictions[key_name[:8]][0]) +
-                             len(quarter_finals_predictions[key_name[8:]][0]) + 8:-1]
 
-                if row[-1] != "\n":
-                    winner += row[-1]
+        for file_1, file_2 in zip(semi_finals, results):
+            if row_number % 4 == 0:
+                key_name = file_1[:8] + file_1[11:-1]
+            if row_number % 4 == 2:
+                first_country = file_1[len(quarter_finals_predictions[key_name[:8]][0]) +
+                                       len(quarter_finals_predictions[key_name[8:]][0]) + 8:-1]
+
+                row = file_2.split("-->")
+                winner = row[-1].strip().strip("\n")
+
+                if file_1[-1] != "\n":
+                    first_country += file_1[-1]
+                validation_check(first_country)
                 validation_check(winner)
 
-                if quarter_finals_predictions[key_name[:8]][0] == winner:
-                    runner_up = quarter_finals_predictions[key_name[8:]][0]
+                if quarter_finals_predictions[key_name[:8]][0] == first_country:
+                    second_country = quarter_finals_predictions[key_name[8:]][0]
                 else:
-                    runner_up = quarter_finals_predictions[key_name[:8]][0]
-                validation_check(runner_up)
-                predictions[key_name] = (winner, runner_up)
+                    second_country = quarter_finals_predictions[key_name[:8]][0]
+                validation_check(second_country)
 
+                if row[0].split("v")[0][:-1] == winner:
+                    loser = row[0].split("v")[1][1:-1]
+                else:
+                    loser = row[0].split("v")[0][:-1]
+                validation_check(loser)
+
+                points = 0
+                if first_country == winner or second_country == winner:
+                    points += 1
+                if second_country == loser or first_country == loser:
+                    points += 1
+
+                predictions[key_name] = (first_country, second_country, points)
             row_number += 1
 
     return predictions
